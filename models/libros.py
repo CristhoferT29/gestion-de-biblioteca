b12d4fd17e2b6cc12f35e83da02f42be2d075968
CristhoferT29 <cristhofertibaquicha@gmail.com>
2025-04-04 16:58:29 -0500

    feat(models) agregar el modelo para gestionar los libros en memoria

---
+++ models/libros.py
@@ -0,0 +1,18 @@
+libros = [
+    {
+        'id': 1,
+        'titulo': 'El Principito',
+        'isbn': '978-3-16-148410-0',
+        'autor': 'Antoine de Saint-Exupéry',
+        'aniopublicacion': 1943,
+    },
+    {
+        'id': 2,
+        'titulo': 'Cien años de soledad',
+        'isbn': '978-3-16-148410-1',
+        'autor': 'Gabriel García Márquez',
+        'aniopublicacion': 1967,
+    },
+]
+
+id = 2
