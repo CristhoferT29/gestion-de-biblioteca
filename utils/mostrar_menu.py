3cfa77a2c2599d7b50ad10433a58d216544ff00c
CristhoferT29 <cristhofertibaquicha@gmail.com>
2025-04-04 17:07:19 -0500

    feat(utils): agregar función para mostrar el menú

---
+++ utils/mostrar_menu.py
@@ -0,0 +1,9 @@
+def menu():
+    print("Biblioteca de Cristhofer T.")
+    print("Bienvenido a la biblioteca")
+    print("Seleccione una opción:")
+    print("1. Agregar libro")
+    print("2. Modificar libro")
+    print("3. Eliminar libro")
+    print("4. Consultar libros")
+    print("5. Salir.")
